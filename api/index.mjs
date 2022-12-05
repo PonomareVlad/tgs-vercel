155dee941d935582d502f918971dae3a12e8a6a2
PonomareVlad <Git@PonomareVlad.ru>
2022-12-06 03:07:28 +0500

    Test install

--- api/index.mjs
+++ api/index.mjs
@@ -2,7 +2,7 @@ import {execSync} from 'node:child_process';
 
 export default async ({body, query, cookies, headers}, {json}) => {
     try {
-        console.debug(execSync('lottie_convert.py').toString());
+        console.debug(execSync('python', {input: '/usr/local/lib/python3.9/site-packages/lottie/bin/lottie_convert.py'}).toString());
     } catch (e) {
         console.error(e);
     }
