bd362930514c6ac572057be2dcdcedf2feefa9e0
Vladislav Ponomarev <Git@PonomareVlad.ru>
2022-12-24 04:19:02 +0500

    In-memory files and JSON errors

--- api/index.py
+++ api/index.py
@@ -1,13 +1,24 @@
-from http.server import BaseHTTPRequestHandler
-from lottie.exporters.core import export_tgs
-from lottie.importers.svg import import_svg
+import sys
 import cgi
-
-svg_path = '/tmp/sticker.svg'
-tgs_path = '/tmp/sticker.svg'
-
+import json
+import gzip
+import logging
+import tempfile
+from lottie.importers.svg import import_svg
+from http.server import BaseHTTPRequestHandler
+from lottie.exporters.tgs_validator import TgsValidator
 
 class handler(BaseHTTPRequestHandler):
+
+    def send_json_error(self, code=500, error=None, message=None):
+        if error is None:
+            exception = sys.exc_info()
+            error = {"type": exception[0].__name__, "message": str(exception[1])}
+        self.send_response(code, message)
+        self.send_header('Content-type', 'text/json; charset=utf-8')
+        self.end_headers()
+        json_obj = json.dumps({"error": error}, indent=4)
+        self.wfile.write(json_obj.encode())
 
     def do_GET(self):
         return self.send_error(405)
@@ -21,14 +32,31 @@ class handler(BaseHTTPRequestHandler):
 
         uploaded_file = form.getvalue("file")
         if uploaded_file:
-            self.send_response(200)
-            self.send_header('Content-type', 'application/gzip')
-            self.send_header('Content-Disposition', 'attachment; filename="sticker.tgs"')
-            self.end_headers()
-            open(svg_path, "wb").write(uploaded_file)
-            animation = import_svg(svg_path)
-            export_tgs(animation, tgs_path, True, True)
-            self.wfile.write(open(tgs_path, 'rb').read())
+            with tempfile.TemporaryFile("wb+") as svg:
+                try:
+                    svg.write(uploaded_file)
+                    animation = import_svg(svg)
+                    animation.tgs_sanitize()
+                    validator = TgsValidator()
+                    validator(animation)
+                    if validator.errors:
+                        details = map(str, validator.errors)
+                        error = {"type": "ValidationError", "message": "Unsupported SVG content", "details": details}
+                        logging.critical(error["message"])
+                        return self.send_json_error(422, error)
+                    lottie_dict = animation.to_dict()
+                    lottie_dict["tgs"] = 1
+                    tgs = gzip.compress(json.dumps(lottie_dict).encode())
+                except Exception as e:
+                    logging.critical(e, exc_info=True)
+                    self.send_json_error(422)
+                else:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'application/gzip')
+                    self.send_header('Content-Disposition', 'attachment; filename="sticker.tgs"')
+                    self.end_headers()
+                    self.wfile.write(tgs)
+
         else:
-            self.send_error(415)
-        return
+            logging.critical("Unsupported file")
+            self.send_json_error(415, {"type": "FileError", "message": "Unsupported file"})
